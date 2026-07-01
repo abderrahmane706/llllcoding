90ffe752ec0be7526afefa6cc5a19cd4e7de48db
abderhmane706 <abderhmane706@gmail.com>
2026-07-01 11:53:18 +0100

    15 commit to add

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -46,7 +46,7 @@ export async function GET(request: Request) {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           cookiesToSet.forEach(({ name, value, options }) => {
             // Write to cookieStore so server-side reads in this request work
             try {
